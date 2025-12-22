6fa7290133142e86678e8924a0a856e3fe202d94
sajadevo <sajadevvo@gmail.com>
2025-12-22 09:46:32 +0400

    add accordion

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Buttons } from "@/app/sections/buttons";
+import { Accordions } from "@/app/sections/accordions";
 
 export default function Home() {
   return (
     <div className="container mx-auto min-h-screen border-x">
+      <Accordions />
       <Buttons />
     </div>
   );
---
+++ components/ui/accordion.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import * as React from "react";
+import { Accordion as BuiAccordion } from "@base-ui/react/accordion";
+
+import { ChevronDownIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const accordionItemVariants = cva(
+  "bg-transparent [corner-shape:var(--shape)]",
+  {
+    variants: {
+      variant: {
+        default: "border-b last:border-b-0",
+        solid: "bg-secondary text-secondary-foreground rounded-lg my-2 px-4",
+        outline: "border text-surface-foreground rounded-lg my-2 px-4",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+type AccordionContextType = VariantProps<typeof accordionItemVariants>;
+
+const AccordionContext = React.createContext<AccordionContextType>({
+  variant: "default",
+});
+
+function useAccordionContext() {
+  const context = React.useContext(AccordionContext);
+
+  if (!context) {
+    throw new Error(
+      "`useAccordionContext()` must be used within the `<Accordion />`",
+    );
+  }
+
+  return context;
+}
+
+function Accordion({
+  variant = "default",
+  ...props
+}: React.ComponentProps<typeof BuiAccordion.Root> &
+  VariantProps<typeof accordionItemVariants>) {
+  return (
+    <AccordionContext.Provider value={{ variant }}>
+      <BuiAccordion.Root data-slot="accordion" {...props} />
+    </AccordionContext.Provider>
+  );
+}
+
+function AccordionItem({
+  className,
+  ...props
+}: React.ComponentProps<typeof BuiAccordion.Item>) {
+  const { variant } = useAccordionContext();
+
+  return (
+    <BuiAccordion.Item
+      data-slot="accordion-item"
+      data-variant={variant}
+      className={cn(accordionItemVariants({ variant, className }))}
+      {...props}
+    />
+  );
+}
+
+function AccordionTrigger({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof BuiAccordion.Trigger>) {
+  return (
+    <BuiAccordion.Header
+      data-slot="accordion-header"
+      className="flex [&[data-open]_[data-slot=accordion-trigger-icon]]:rotate-180"
+    >
+      <BuiAccordion.Trigger
+        data-slot="accordion-trigger"
+        className={cn(
+          "flex flex-1 items-start justify-between gap-4 py-4 text-start text-sm font-medium transition-all disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        {...props}
+      >
+        {children}
+        <ChevronDownIcon
+          data-slot="accordion-trigger-icon"
+          className="text-surface-foreground/50 pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200"
+        />
+      </BuiAccordion.Trigger>
+    </BuiAccordion.Header>
+  );
+}
+
+function AccordionPanel({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof BuiAccordion.Panel>) {
+  return (
+    <BuiAccordion.Panel
+      data-slot="accordion-panel"
+      className="h-(--accordion-panel-height) overflow-hidden text-sm transition-[height] data-ending-style:h-0 data-starting-style:h-0"
+      {...props}
+    >
+      <div className={cn("pt-0 pb-4", className)}>{children}</div>
+    </BuiAccordion.Panel>
+  );
+}
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionPanel };
