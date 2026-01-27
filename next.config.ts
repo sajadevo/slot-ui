9cb933b2084bc93fcdda8f5c18f3ee359600825d
sajadevo <sajadevvo@gmail.com>
2026-01-27 08:21:10 +0400

    add fumadocs, next-themes, and rehype-pretty-code dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
+import { createMDX } from "fumadocs-mdx/next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
