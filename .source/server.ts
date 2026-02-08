d00285f490d418a98028f24f9acde22c7f2d4aa3
sajadevo <sajadevvo@gmail.com>
2026-02-09 01:11:21 +0400

    add get started docs page

--- .source/server.ts
+++ .source/server.ts
@@ -1,26 +1,27 @@
 // @ts-nocheck
-import * as __fd_glob_21 from "../content/docs/components/tooltip.mdx?collection=docs"
-import * as __fd_glob_20 from "../content/docs/components/toggle.mdx?collection=docs"
-import * as __fd_glob_19 from "../content/docs/components/toggle-group.mdx?collection=docs"
-import * as __fd_glob_18 from "../content/docs/components/tabs.mdx?collection=docs"
-import * as __fd_glob_17 from "../content/docs/components/switch.mdx?collection=docs"
-import * as __fd_glob_16 from "../content/docs/components/slider.mdx?collection=docs"
-import * as __fd_glob_15 from "../content/docs/components/skeleton.mdx?collection=docs"
-import * as __fd_glob_14 from "../content/docs/components/separator.mdx?collection=docs"
-import * as __fd_glob_13 from "../content/docs/components/radio.mdx?collection=docs"
-import * as __fd_glob_12 from "../content/docs/components/progress.mdx?collection=docs"
-import * as __fd_glob_11 from "../content/docs/components/popover.mdx?collection=docs"
-import * as __fd_glob_10 from "../content/docs/components/label.mdx?collection=docs"
-import * as __fd_glob_9 from "../content/docs/components/input.mdx?collection=docs"
-import * as __fd_glob_8 from "../content/docs/components/dialog.mdx?collection=docs"
-import * as __fd_glob_7 from "../content/docs/components/collapsible.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/components/checkbox.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/components/checkbox-group.mdx?collection=docs"
-import * as __fd_glob_4 from "../content/docs/components/button.mdx?collection=docs"
-import * as __fd_glob_3 from "../content/docs/components/avatar.mdx?collection=docs"
-import * as __fd_glob_2 from "../content/docs/components/alert-dialog.mdx?collection=docs"
-import * as __fd_glob_1 from "../content/docs/components/accordion.mdx?collection=docs"
-import * as __fd_glob_0 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_22 from "../content/docs/components/tooltip.mdx?collection=docs"
+import * as __fd_glob_21 from "../content/docs/components/toggle.mdx?collection=docs"
+import * as __fd_glob_20 from "../content/docs/components/toggle-group.mdx?collection=docs"
+import * as __fd_glob_19 from "../content/docs/components/tabs.mdx?collection=docs"
+import * as __fd_glob_18 from "../content/docs/components/switch.mdx?collection=docs"
+import * as __fd_glob_17 from "../content/docs/components/slider.mdx?collection=docs"
+import * as __fd_glob_16 from "../content/docs/components/skeleton.mdx?collection=docs"
+import * as __fd_glob_15 from "../content/docs/components/separator.mdx?collection=docs"
+import * as __fd_glob_14 from "../content/docs/components/radio.mdx?collection=docs"
+import * as __fd_glob_13 from "../content/docs/components/progress.mdx?collection=docs"
+import * as __fd_glob_12 from "../content/docs/components/popover.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/components/label.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/components/input.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/components/dialog.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/components/collapsible.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/components/checkbox.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/components/checkbox-group.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/components/button.mdx?collection=docs"
+import * as __fd_glob_4 from "../content/docs/components/avatar.mdx?collection=docs"
+import * as __fd_glob_3 from "../content/docs/components/alert-dialog.mdx?collection=docs"
+import * as __fd_glob_2 from "../content/docs/components/accordion.mdx?collection=docs"
+import * as __fd_glob_1 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_0 from "../content/docs/get-started.mdx?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
 
@@ -29,4 +30,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {}, {"index.mdx": __fd_glob_0, "components/accordion.mdx": __fd_glob_1, "components/alert-dialog.mdx": __fd_glob_2, "components/avatar.mdx": __fd_glob_3, "components/button.mdx": __fd_glob_4, "components/checkbox-group.mdx": __fd_glob_5, "components/checkbox.mdx": __fd_glob_6, "components/collapsible.mdx": __fd_glob_7, "components/dialog.mdx": __fd_glob_8, "components/input.mdx": __fd_glob_9, "components/label.mdx": __fd_glob_10, "components/popover.mdx": __fd_glob_11, "components/progress.mdx": __fd_glob_12, "components/radio.mdx": __fd_glob_13, "components/separator.mdx": __fd_glob_14, "components/skeleton.mdx": __fd_glob_15, "components/slider.mdx": __fd_glob_16, "components/switch.mdx": __fd_glob_17, "components/tabs.mdx": __fd_glob_18, "components/toggle-group.mdx": __fd_glob_19, "components/toggle.mdx": __fd_glob_20, "components/tooltip.mdx": __fd_glob_21, });
+export const docs = await create.docs("docs", "content/docs", {}, {"get-started.mdx": __fd_glob_0, "index.mdx": __fd_glob_1, "components/accordion.mdx": __fd_glob_2, "components/alert-dialog.mdx": __fd_glob_3, "components/avatar.mdx": __fd_glob_4, "components/button.mdx": __fd_glob_5, "components/checkbox-group.mdx": __fd_glob_6, "components/checkbox.mdx": __fd_glob_7, "components/collapsible.mdx": __fd_glob_8, "components/dialog.mdx": __fd_glob_9, "components/input.mdx": __fd_glob_10, "components/label.mdx": __fd_glob_11, "components/popover.mdx": __fd_glob_12, "components/progress.mdx": __fd_glob_13, "components/radio.mdx": __fd_glob_14, "components/separator.mdx": __fd_glob_15, "components/skeleton.mdx": __fd_glob_16, "components/slider.mdx": __fd_glob_17, "components/switch.mdx": __fd_glob_18, "components/tabs.mdx": __fd_glob_19, "components/toggle-group.mdx": __fd_glob_20, "components/toggle.mdx": __fd_glob_21, "components/tooltip.mdx": __fd_glob_22, });
