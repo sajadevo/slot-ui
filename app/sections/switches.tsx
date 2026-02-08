d23c9240f7d36de49887dbb96c15e473f8f23be2
sajadevo <sajadevvo@gmail.com>
2026-02-09 02:42:37 +0400

    remove old playground page and sections

--- app/sections/switches.tsx
+++
@@ -1,9 +0,0 @@
-import { Switch } from "@/components/ui/switch";
-
-export function Switches() {
-  return (
-    <section className="flex justify-center border-b p-12">
-      <Switch />
-    </section>
-  );
-}
